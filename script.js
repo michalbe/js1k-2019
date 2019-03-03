4393006e33dfe32eb37b2607abf5fab71a664f2d
Michał Budzyński <michal@virtualdesign.pl>
2019-03-03 17:25:58 +0800

    1023b

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-var M = Math,
-    Mcos = M.cos,
-    CIRCLE = M.PI * 2,
-    wall_text = 'JS1K',
-    RayMap_walls = "e0vm631d,aerab41j,akl6qrjj,72eh1s8j,d5hasum7"
-    .split(',')
-    .map((e) => parseInt(e, 36).toString(2)).join('').split(''),
-    RayMap_width = 10,
-    RayMap_height = 20,
-    RayCamera_fov = M.PI * 0.4,
-    RayCamera_range = 9,
+// Based on a canvas raycaster build by Laerin
+// https://codepen.io/xgundam05/pen/vcBbL
+
+var CIRCLE = Math.PI * 2;
+const wall_text = 'JS1K';
+let RayMap_walls = parseInt("axgulq06kf", 36).toString(2).split(''),
+    RayMap_width = 5,
+    RayMap_height = 10,
+    RayCamera_fov = Math.PI * 0.4,
+    RayCamera_range = 14,
     RayCamera_lightRange = 5,
-    p_x,p_y,
-    RayCamera_p_x = p_x = 2,
-    RayCamera_p_y = p_y = 2,
+    p_x, p_y,
+    RayCamera_p_x = p_x = 3,
+    RayCamera_p_y = p_y = 4,
     dir,
     RayCamera_dir = dir = 0,
     RaycastRenderer_height = 9,
     RaycastRenderer_resolution = 28,
     // 37 - l, 39 - r, 38 - f, 40 - b
-    Controls_states = { 7: 0, 9: 0, 8: 0, 10: 0 },
+    Controls_states = [],
     lastTime = 0,
     str = '',
     rot = (angle) => {
@@ -26,18 +25,18 @@ var M = Math,
         RayCamera_dir = dir;
     },
     walk = (distance) => {
-        var dx = Mcos(dir) * distance;
-        var dy = M.sin(dir) * distance;
+        var dx = Math.cos(dir) * distance;
+        var dy = Math.sin(dir) * distance;
         if (RayMap_Get(p_x + dx, p_y) <= 0) p_x += dx;
         if (RayMap_Get(p_x, p_y + dy) <= 0) p_y += dy;
         RayCamera_p_x = p_x;
         RayCamera_p_y = p_y;
     },
     update = (seconds) => {
-        if (Controls_states[7]) rot(-M.PI * seconds);
-        if (Controls_states[9]) rot(M.PI * seconds);
-        if (Controls_states[8]) walk(3 * seconds);
-        if (Controls_states[10]) walk(-3 * seconds);
+        if (Controls_states[0]) rot(-Math.PI * seconds);
+        if (Controls_states[2]) rot(Math.PI * seconds);
+        if (Controls_states[1]) walk(3 * seconds);
+        if (Controls_states[3]) walk(-3 * seconds);
     };
 
 function RayMap_Get(x, y) {
@@ -48,11 +47,11 @@ function RayMap_Get(x, y) {
 };
 
 function RayMap_Raycast(x, y, angle, range) {
-    var cells = [],
-        sin = M.sin(angle),
-        cos = Mcos(angle),
-        stepX, stepY, nextStep;
+    var cells = [];
+    var sin = Math.sin(angle);
+    var cos = Math.cos(angle);
 
+    var stepX, stepY, nextStep;
     nextStep = { x, y, cell: 0, distance: 0 };
     do {
         cells.push(nextStep);
@@ -69,7 +68,7 @@ function RayMap_Raycast(x, y, angle, range) {
 };
 
 function RayMap___step(rise, run, x, y, inverted) {
-    var dx = run > 0 ? ~~(x + 1) - x : M.ceil(x - 1) - x;
+    var dx = run > 0 ? ~~(x + 1) - x : Math.ceil(x - 1) - x;
     var dy = dx * rise / run;
     return (run === 0) ? { length2: 9 } : {
         x: inverted ? y + dy : x + dx,
@@ -82,8 +81,8 @@ function RayMap___inspect(step, shiftX, shiftY, distance, cos, sin) {
     var dx = cos < 0 ? shiftX : 0;
     var dy = sin < 0 ? shiftY : 0;
     var index = (((step.y - dy) | 0) * RayMap_width) + ((step.x - dx) | 0);
-    step.cell = (index < 0 || index >= RayMap_width * RayMap_height) ? -1 : RayMap_walls[index];
-    step.distance = distance + M.sqrt(step.length2);
+    step.cell = (index < 0 || index >= 440) ? -1 : RayMap_walls[index];
+    step.distance = distance + Math.sqrt(step.length2);
     return step;
 };
 
@@ -91,9 +90,9 @@ function RayCamera_Rotate(angle) {
     RayCamera_dir = (RayCamera_dir + angle + CIRCLE) % (CIRCLE);
 }
 
-function RaycastRenderer___project(angle, distance) {
-    var z = distance * Mcos(angle);
-    var wallHeight = RaycastRenderer_height / z;
+function RaycastRenderer___project(height, angle, distance) {
+    var z = distance * Math.cos(angle);
+    var wallHeight = RaycastRenderer_height * height / z;
     return wallHeight;
 };
 
@@ -103,8 +102,8 @@ function RaycastRenderer___drawColumn(column, ray, angle) {
 
     if (hit < ray.length) {
         var step = ray[hit];
-        var wall = RaycastRenderer___project(angle, step.distance);
-        const alpha = 1 - M.max((step.distance) / RayCamera_lightRange, 0);
+        var wall = RaycastRenderer___project(1, angle, step.distance);
+        const alpha = 1 - Math.max((step.distance) / RayCamera_lightRange, 0);
         els[column].style.opacity = alpha;
         els[column].style.transform = 'scaleY(' + wall + ')';
     }
@@ -112,35 +111,32 @@ function RaycastRenderer___drawColumn(column, ray, angle) {
 
 function RaycastRenderer___drawColumns() {
     for (var col = 0; col < RaycastRenderer_resolution; col++) {
-        var angle = RayCamera_fov * (col / RaycastRenderer_resolution - 0.5),
-        ray = RayMap_Raycast(RayCamera_p_x, RayCamera_p_y, RayCamera_dir + angle, RayCamera_range);
+        var angle = RayCamera_fov * (col / RaycastRenderer_resolution - 0.5);
+        var ray = RayMap_Raycast(RayCamera_p_x, RayCamera_p_y, RayCamera_dir + angle, RayCamera_range);
         RaycastRenderer___drawColumn(col, ray, angle);
     }
 };
 
 
-function Controls_onKey(val) {
+function Controls_onKey(val, e) {
     return (e) => {
-        Controls_states[e.keyCode%30] = val;
+        Controls_states[e.keyCode%37] = val;
     }
 };
 
 for (let i = 0; i < RaycastRenderer_resolution; i++) {
-    str += '<p style="display:inline-block;">' + wall_text[i % wall_text.length] + '</p>';
+    str += '<p style="display:inline-block;margin-top:99px">' + wall_text[i % wall_text.length] + '</p>';
 }
 
 b.innerHTML = str;
-b.style.margin = '59px';
 let els = d.querySelectorAll('p');
 
 d.onkeydown = Controls_onKey(1);
 d.onkeyup = Controls_onKey(0);
 
-function UpdateRender(time) {
-    var seconds = (time - lastTime) / 1000;
-    lastTime = time;
+function UpdateRender() {
+    var seconds = 0.004;
     update(seconds);
     RaycastRenderer___drawColumns();
-    requestAnimationFrame(UpdateRender);
 }
-UpdateRender();
+setInterval(UpdateRender);
